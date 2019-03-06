4d7b1973f2fc060ea56b9ce4a896cae33646d11a
ShifaHaider <shifa.haider065@gmail.com>
2019-03-06 03:27:57 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link, Redirect} from "react-router-dom";
+import Main from './routes/Main/main';
+import Register from './routes/Register/register';
+import Login from './routes/Login/login';
+
+var userID = localStorage.getItem('userID');
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <div>
+                        <Route path="/" exact component={Login}/>
+                        <Route path="/register" exact render={() => (userID ? <Redirect to='/main'/> : <Register/>)}/>
+                        <Route path="/login" exact render={() => (userID ? <Redirect to='/main'/> : <Login/>)}/>
+                        <Route path="/main" exact render={() => (!userID ? <Redirect to='/login'/> : <Main/>)}/>
+                    </div>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/routes/Register/register.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Register extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Register</h1>
+            </div>
+        );
+    }
+}
+
+export default Register;
